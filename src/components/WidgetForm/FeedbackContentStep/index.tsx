4f5ddd28b1d3ee5ac448290ed85d8003602aa31f
Bruno Cardoso <brunocpessoal@gmail.com>
2022-05-04 20:48:24 -0300

    add ScreenshotButton in FeedbackContentStep

--- src/components/WidgetForm/FeedbackContentStep/index.tsx
+++ src/components/WidgetForm/FeedbackContentStep/index.tsx
@@ -1,6 +1,9 @@
-import { ArrowLeft, Camera } from 'phosphor-react';
+import { useState } from 'react';
+
+import { ArrowLeft } from 'phosphor-react';
 
 import { CloseButton } from '@/components/CloseButton';
+import { ScreenshotButton } from '@/components/ScreenshotButton';
 import { FeedbackType, feedbackTypes } from '@/components/WidgetForm';
 
 type FeedbackContentStepProps = {
@@ -12,6 +15,8 @@ function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested,
 }: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
   return (
@@ -44,12 +49,10 @@ function FeedbackContentStep({
         />
 
         <footer className="flex gap-2 mt-2">
-          <button
-            type="button"
-            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-          >
-            <Camera className="w-6 h-6" />
-          </button>
+          <ScreenshotButton
+            onScreenshotTook={setScreenshot}
+            screenshot={screenshot}
+          />
 
           <button
             type="submit"
